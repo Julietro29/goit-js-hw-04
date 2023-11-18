5b8b2064f7134a3643d7f31f8352296429de9ea6
Julietro29 <mousemiu1@gmail.com>
2023-11-18 02:08:58 +0200

    Редактировано 2 задание

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,10 @@
 function calcAverageCalories(days) {
+  if (days.length === 0) {
+    return 0;
+  }
     let totalCalories = 0;
     for (let day of days) {
-        totalCalories += day.calories;
+    totalCalories += day.calories;
     }
     const averageCalories = totalCalories / days.length;
     return averageCalories;
